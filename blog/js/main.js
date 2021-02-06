cfbb4194fa491ff013744a91eed6f289952e5263
仁志 <legend1world@gmail.com>
2021-02-06 19:27:16 +0900

    up

---
+++ blog/js/main.js
@@ -0,0 +1,21 @@
+		$(function() {
+    var topBtn = $('#page_top');
+    topBtn.hide();
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
